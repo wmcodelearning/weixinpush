fbe8f93966afdd05f908f748077574051dc475b0
wmcodelearning <73421987+wmcodelearning@users.noreply.github.com>
2022-09-28 15:31:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from datetime import datetime, date
 from zhdate import ZhDate
 import sys
 import os
- 
- 
+
+
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
     color_list = get_colors(100)
     return random.choice(color_list)
- 
- 
+
+
 def get_access_token():
     # appId
     app_id = config["app_id"]
@@ -29,42 +29,47 @@ def get_access_token():
         sys.exit(1)
     # print(access_token)
     return access_token
- 
- 
+
+
 def get_weather(region):
-   """
-   headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, "eed6f0049f1243cebbcb6bd7eebf28e8")
-    response = get(region_url, headers=headers).json()
-    if response["code"] == "404":
-        print("推送消息失败，请检查地区名是否有误！")
-        os.system("pause")
-        sys.exit(1)
-    elif response["code"] == "401":
-        print("推送消息失败，请检查和风天气key是否正确！")
-        os.system("pause")
-        sys.exit(1)
-    else:
-        # 获取地区的location--id
-        location_id = response["location"][0]["id"]
-    weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
-    response = get(weather_url, headers=headers).json()
-    """
-    # 天气
     weather = "晴天"
-    #response["now"]["text"]
-    # 当前温度
     temp = "30"
-    #response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    # response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
-    wind_dir = "东北风"#response["now"]["windDir"]
+    wind_dir = "东北风"  # response["now"]["windDir"]
     return weather, temp, wind_dir
- 
- 
+    # response["now"]["text"]
+    # 当前温度
+
+
+
+"""
+headers = {
+     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+ }
+ key = config["weather_key"]
+ region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, "eed6f0049f1243cebbcb6bd7eebf28e8")
+ response = get(region_url, headers=headers).json()
+ if response["code"] == "404":
+     print("推送消息失败，请检查地区名是否有误！")
+     os.system("pause")
+     sys.exit(1)
+ elif response["code"] == "401":
+     print("推送消息失败，请检查和风天气key是否正确！")
+     os.system("pause")
+     sys.exit(1)
+ else:
+     # 获取地区的location--id
+     location_id = response["location"][0]["id"]
+ weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
+ response = get(weather_url, headers=headers).json()
+ """
+
+
+# 天气
+
+
 def get_birthday(birthday, year, today):
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
@@ -82,7 +87,7 @@ def get_birthday(birthday, year, today):
         birthday_day = birthday.day
         # 今年生日
         year_date = date(year, birthday_month, birthday_day)
- 
+
     else:
         # 获取国历生日的今年对应月和日
         birthday_month = int(birthday.split("-")[1])
@@ -104,8 +109,8 @@ def get_birthday(birthday, year, today):
         birth_date = year_date
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     return birth_day
- 
- 
+
+
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
     headers = {
@@ -117,8 +122,8 @@ def get_ciba():
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch, note_en
- 
- 
+
+
 def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
@@ -204,8 +209,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         print("推送消息成功")
     else:
         print(response)
- 
- 
+
+
 if __name__ == "__main__":
     try:
         with open("config.txt", encoding="utf-8") as f:
@@ -218,7 +223,7 @@ if __name__ == "__main__":
         print("推送消息失败，请检查配置文件格式是否正确")
         os.system("pause")
         sys.exit(1)
- 
+
     # 获取accessToken
     accessToken = get_access_token()
     # 接收的用户
